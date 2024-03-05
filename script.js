0de0b1148e4dee6c8ec227ac29766c9f2733d6fb
Murali Prasath <70134163+MuraliPrasath-45@users.noreply.github.com>
2024-03-05 19:34:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let obj1={
+1.  let obj1={
     name:"Person",age:5
 };
 
@@ -6,4 +6,28 @@ let obj2={
     age=5,name:"Person 1"
 };
 JSON.stringify(obj1)==JSON.stringify(obj2)
-_.isEqual(obj1,obj2)
+.isEqual(obj1,obj2)
+
+2. var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    for(let i = 0 ; i< result.length ; i++){
+        console.log(result[i].flags.png);   
+}
+}
+
+3. 
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    for(let i = 0 ; i< result.length ; i++){
+          console.log(result[i].name);
+          console.log(result[i].region);
+          console.log(result[i].subregion); 
+          console.log(result[i].population);     
+}
+}
